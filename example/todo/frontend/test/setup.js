64614e38924de003742f830c565835f6ae7fbc11
John Lees-Miller <jdleesmiller@gmail.com>
2019-08-09 22:12:18 +0100

    Remove Date workaround after patch

--- example/todo/frontend/test/setup.js
+++ example/todo/frontend/test/setup.js
@@ -1,10 +1,7 @@
 require('jsdom-global')('', {
   // We rely on window.location.origin, which is null with the default
   // about:blank url.
-  url: 'http://example.com',
-  // Work around missing window.Date
-  // https://github.com/testing-library/dom-testing-library/issues/194
-  runScripts: 'outside-only'
+  url: 'http://example.com'
 })
 
 global.fetch = require('whatwg-fetch').fetch
